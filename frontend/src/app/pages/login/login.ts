0d5d706035f9a81c4ee41c91f9f3b36f4741d0c5
Daniel Teres <daniel.teres@tdconsulting.es>
2025-10-27 10:13:58 +0100

    Minor spelling changes done

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,29 +1,40 @@
 import { Component } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MessageService } from 'primeng/api';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from "../../services/authService.service";
-
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
-  providers: [MessageService]
+  styleUrls: ['./login.scss'],
+  providers: []
 })
 export class Login {
-  user = {
-    userName: null,
-    password: null
-  };
+  [key: string]: any;
+  username: string = '';
+  password: string = '';
+  loginValid: boolean = true;
 
-  constructor(private messageService: MessageService,
-    private authService: AuthService
-  ) { }
+  constructor(private authService: AuthService) { }
 
-  onSubmit(user: any) {
-    this.authService.login(user).subscribe();
+  onSubmit() {
+    this.authService.login({ userName: this.username, password: this.password })
+      .subscribe({
+        next: res => this.loginValid = true,
+        error: err => this.loginValid = false
+      });
   }
 }
